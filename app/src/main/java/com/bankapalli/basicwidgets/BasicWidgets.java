858d7cdddf9e52e8d29943f5e250155724dbcec7
Venkata.bankapalli <venkata.bankapalli@kony.com>
2018-02-12 18:28:50 +0530

    Initial commit

---
+++ app/src/main/java/com/bankapalli/basicwidgets/BasicWidgets.java
@@ -0,0 +1,13 @@
+package com.bankapalli.basicwidgets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BasicWidgets extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_widgets);
+    }
+}
